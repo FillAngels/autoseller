66b6e176b295f221cfb577f6356ae926f83fca43
FillAngels <105607546+FillAngels@users.noreply.github.com>
2022-05-16 00:19:54 +0400

    Add files via upload

--- AutoSeller.lua
+++ AutoSeller.lua
@@ -108,7 +108,9 @@ function main()
 	if not isSampfuncsLoaded() or not isSampLoaded() then return end
     while not isSampAvailable() do wait(100) end 
     
-    if not doesFileExist(getWorkingDirectory()..'\\config\\AutoSeller.ini') then inicfg.save(cfg, 'AutoSeller.ini') end
+    autoupdate("https://raw.githubusercontent.com/FillAngels/autoseller/main/AutoSeller.lua", '['..string.upper(thisScript().name)..']: ', "vk.com/homkarp993")
+	
+	if not doesFileExist(getWorkingDirectory()..'\\config\\AutoSeller.ini') then inicfg.save(cfg, 'AutoSeller.ini') end
 
     sampAddChatMessage("[AutoSeller]: {FFFFFF}Скрипт загружен, для настройки введите /as", 0x5CBCFF)
 
@@ -119,8 +121,11 @@ function main()
 	sampRegisterChatCommand('srep',function()
         sampSendChat('/o Администрация ждёт ваших репортов! (/mm > Репорт)')
     end)
-	
-    writeMemory(7634870, 1, 0, 0)
+	sampRegisterChatCommand('1rep',function()
+        sampSendChat('/o Администрация ждёт ваших репортов! (/mm > Репорт)')
+    end)
+    
+	writeMemory(7634870, 1, 0, 0)
     writeMemory(7635034, 1, 0, 0)
     memory.hex2bin('5051FF1500838500', 7623723, 8)
     memory.hex2bin('0F847B010000', 5499528, 6)
@@ -299,6 +304,62 @@ function reloading()
 	sampAddChatMessage("Џерезагрузка!!", 0x5CBCFF)
 end
 	
+function autoupdate(json_url, prefix, url)
+  local dlstatus = require('moonloader').download_status
+  local json = getWorkingDirectory() .. '\\'..thisScript().name..'-version.json'
+  if doesFileExist(json) then os.remove(json) end
+  downloadUrlToFile(json_url, json,
+    function(id, status, p1, p2)
+      if status == dlstatus.STATUSEX_ENDDOWNLOAD then
+        if doesFileExist(json) then
+          local f = io.open(json, 'r')
+          if f then
+            local info = decodeJson(f:read('*a'))
+            updatelink = info.updateurl
+            updateversion = info.latest
+            f:close()
+            os.remove(json)
+            if updateversion ~= thisScript().version then
+              lua_thread.create(function(prefix)
+                local dlstatus = require('moonloader').download_status
+                local color = -1
+                sampAddChatMessage((prefix..'Обнаружено обновление. Пытаюсь обновиться c '..thisScript().version..' на '..updateversion), color)
+                wait(250)
+                downloadUrlToFile(updatelink, thisScript().path,
+                  function(id3, status1, p13, p23)
+                    if status1 == dlstatus.STATUS_DOWNLOADINGDATA then
+                      print(string.format('Загружено %d из %d.', p13, p23))
+                    elseif status1 == dlstatus.STATUS_ENDDOWNLOADDATA then
+                      print('Загрузка обновления завершена.')
+                      sampAddChatMessage((prefix..'Обновление завершено!'), color)
+                      goupdatestatus = true
+                      lua_thread.create(function() wait(500) thisScript():reload() end)
+                    end
+                    if status1 == dlstatus.STATUSEX_ENDDOWNLOAD then
+                      if goupdatestatus == nil then
+                        sampAddChatMessage((prefix..'Обновление прошло неудачно. Запускаю устаревшую версию..'), color)
+                        update = false
+                      end
+                    end
+                  end
+                )
+                end, prefix
+              )
+            else
+              update = false
+              print('v'..thisScript().version..': Обновление не требуется.')
+            end
+          end
+        else
+          print('v'..thisScript().version..': Не могу проверить обновление. Смиритесь или проверьте самостоятельно на '..url)
+          update = false
+        end
+      end
+    end
+  )
+  while update ~= false do wait(100) end
+end
+
 function cmd_imgui()
 	main_window_state.v = not main_window_state.v
     imgui.Process = main_window_state.v
